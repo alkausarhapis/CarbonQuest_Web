d6de4d36bf27e8cb99690389e737918c1222a321
alkausarhapis <alkausarhapis@gmail.com>
2025-10-30 18:46:47 +0700

    fix: logout logic

--- page_bintang/script.js
+++ page_bintang/script.js
@@ -151,7 +151,7 @@
   if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
       if (confirm("Are you sure you want to logout?")) {
-        window.location.href = "../page_Hizkia/index.html";
+        window.location.href = "../page_Syarif/index.html";
       }
     });
   }
